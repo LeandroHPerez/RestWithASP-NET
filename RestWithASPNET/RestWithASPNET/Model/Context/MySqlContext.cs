7c62582dbb732981a4aab157b33870c90c1c9bad
Leandro Perez <leandropremier@gmail.com>
2019-03-07 13:40:47 -0300

    Include db layer

---
+++ RestWithASPNET/RestWithASPNET/Model/Context/MySqlContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        //Mapear a tabela Persons através do objeto persons
+        public DbSet<Person> Persons { get; set; }
+    }
+}
+
--- RestWithASPNET/RestWithASPNET/Startup.cs
+++ RestWithASPNET/RestWithASPNET/Startup.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using RestWithASPNET.Model.Context;
 using RestWithASPNET.Services.Implementations;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace RestWithASPNET
 {
@@ -26,6 +23,17 @@ namespace RestWithASPNET
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"]; //vai pegar o caminho de conexão do arquico appsettings.json
+
+            //services.AddDbContext<MySqlContext>(options => options.UseMySql(connection));
+
+            services.AddDbContext<MySQLContext>(options => options.UseSqlServer(connection));
+            //services.AddDbContext<MySQLContext>(optionsBuilder => optionsBuilder.UseMySQL(connection));
+
+            //services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+
+            //Framework MVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             //Injeção de dependência
